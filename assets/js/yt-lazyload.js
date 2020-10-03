7705eb8fda188ea1c547e39a5a9c64f94ca9b6fc
Francesco De Feo <22840336+hollow-frenk@users.noreply.github.com>
2020-10-03 05:20:21 +0200

    up

--- assets/js/yt-lazyload.js
+++ assets/js/yt-lazyload.js
@@ -21,10 +21,10 @@
         template_iframe,
         
         settings_observer_rootMargin    = '200px 0px',                  //Intersection Observer API option - rootMargin (Y, X)
-        settings_thumb_base_url         = 'https://i.ytimg.com/COIauzEjwU8', 'https://i.ytimg.com/cxPcFQTDaBw'    //Base URL where thumbnails are stored
+        settings_thumb_base_url         = 'https://i.ytimg.com/vi/C8bbQDRgPR8/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&amp;rs=AOn4CLBGwXL6q_rgnaXNmjP2iBpo3JGY0Q',    //Base URL where thumbnails are stored
         settings_thumb_extension        = 'jpg';                        //Thumbnail extension
         
-    
+        
     
     /********************************************************************
     ************************ IF ELEMENTS EXIST **************************
